f3fcad9d1680249576fedc719ebff7a787b65fec
ruiliny <39702239+ruiliny@users.noreply.github.com>
2020-02-19 22:44:35 +1100

    Update main.c

--- main.c
+++ main.c
@@ -1,37 +1,104 @@
 /***************************************************************************
  *
  *   File        : main.c
- *   Student Id  : <INSERT STUDENT ID HERE>
- *   Name        : <INSERT STUDENT NAME HERE>
+ *   Student Id  : <854255>
+ *   Name        : <Ce ZHANG>
  *
  ***************************************************************************/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
- //#include <sys/time.h>
+//#include <sys/time.h>
 #include <string.h>
+#include <stdint.h>
+#include <assert.h>
 #include "tasks.h"
 
+char* out_file_name1 = "task1.csv";
+char* out_file_name2 = "task2.csv";
+char* out_file_name3 = "task3.csv";
+char* out_file_name4 = "task4.csv";
+
+
 int main(int argc, char *argv[]) {
-
+	
+	char* file_name = argv[1];
 	/* TODO: Parse Command Line Arguments */
-	char* flow_file = NULL;
-	int resolution = 0;
-
-	/* TODO: Add timing for each task and output running time in ms */
-
+	
+	
+    
 	/* Task 1: Find the maximum velocity difference */
-	//maxveldiff(flow_file);
+	FILE* fp = fopen(file_name, "r");
+	FILE* fp1 = safe_fopen(out_file_name1, "w");
+	FILE* fp2 = safe_fopen(out_file_name2, "w");
+	FILE* fp3 = safe_fopen(out_file_name3, "w");
+	FILE* fp4 = safe_fopen(out_file_name4, "w");
 
+	int line_num = lines(fp);//340230
+	rewind(fp);
+
+	//struct timeval start;
+	//struct timeval stop;
+
+	//gettimeofday(&start, NULL);
+
+	initialization(fp, fp1, line_num);
+
+	/*gettimeofday(&stop, NULL);
+	double elapsed_ms = (stop.tv_sec - start.tv_sec) * 1000.0;
+	elapsed_ms += (stop.tv_usec - start.tv_usec) / 1000.0;
+	printf("TASK 1:  %.2f milliseconds\n", elapsed_ms);
+	*/
 	/* Task 2: Coarser Grid */
-	coarsegrid(flow_file, resolution);
+	int k = atoi(argv[2]);
+	
+	
 
-	/* Task 3: Statistics */
-	//velstat(flow_file);
+	/*gettimeofday(&start, NULL);
+	coarsegrid(fp2, line_num, k);
+	gettimeofday(&stop, NULL);
+    elapsed_ms = (stop.tv_sec - start.tv_sec) * 1000.0;
+    elapsed_ms += (stop.tv_usec - start.tv_usec) / 1000.0;
+    printf("TASK 2:  %.2f milliseconds\n", elapsed_ms);
+	*/
+	/* Task 3: Searching */
 
-	/* Task 4: Wake height and visualisation */
-	//wakevis(flow_file);
+	/*gettimeofday(&start, NULL);
+	searching(fp3, line_num);
+	gettimeofday(&stop, NULL);
+    elapsed_ms = (stop.tv_sec - start.tv_sec) * 1000.0;
+    elapsed_ms += (stop.tv_usec - start.tv_usec) / 1000.0;
+   	printf("TASK 3:  %.2f milliseconds\n", elapsed_ms);
+   */ 
+	/* Task 4: Statistics */
+
+
+
+
+	//gettimeofday(&start, NULL);
+	vortcalc(fp4, line_num);
+	/*gettimeofday(&stop, NULL);
+	elapsed_ms = (stop.tv_sec - start.tv_sec) * 1000.0;
+	elapsed_ms += (stop.tv_usec - start.tv_usec) / 1000.0;
+	printf("TASK 4:  %.2f milliseconds\n", elapsed_ms);
+	*/
+
+	fclose(fp);
+	fclose(fp1);
+	fclose(fp2);
+	fclose(fp3);
+	fclose(fp4);
+	out_file_name1 = NULL;
+	out_file_name2 = NULL;
+	out_file_name3 = NULL;
+	out_file_name4 = NULL;
+	file_name = NULL;
+
+	
+	
+	return (EXIT_SUCCESS);
+}
+
 
 	return (EXIT_SUCCESS);
 }
